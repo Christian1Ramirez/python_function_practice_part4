85b4fb847cf04918d2287b31b66449d7750f798c
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-08-04 18:19:41 -0700

    func1 complete

---
+++ func1.py
@@ -0,0 +1,11 @@
+# Write a Python function called max_num()to find the maximum of three numbers.
+
+def max_num(a, b, c):
+    return max(a, b, c)
+
+a = 10
+b = 14
+c = 50
+
+print(max_num(a, b, c))
+    
